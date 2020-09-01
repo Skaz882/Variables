878cd3ab2b538ecee622aaaf84115412b24f270e
Skaz882 <60199239+Skaz882@users.noreply.github.com>
2020-08-31 20:48:20 -0500

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val byte = 2
+    val short = 16
+    val int = 34
+    val long = 80L
+    val float = 2.222222
+    val double = 88.462
+    val myBoolean = true
+    val char = "s"
+
+    println("A byte is an 8 bit integer, like $byte. \n")
+    println("A short is a 16 bit integer like $short. \n")
+    println("An int is a 32 bit integer like $int. \n")
+    println("A long is a 64 bit integer like $long. \n")
+    println("A float is a small number with a decimal point like $float. \n")
+    println("A double is a large number with a decimal point like $double. \n")
+    println("myBoolean is a true or false value like $myBoolean. \n")
+    println("A char is a single 16 bit Unicode character like $char. \n")
+
+}
